016b9d22343f8d58f31eec64cbf3cf8cc49d29ab
SAMPROO <sampo.tuisku@gmail.com>
2017-09-29 14:14:21 +0300

    SimulatorTest

--- rocketsimulator/src/rocketsimulator/Simulator.java
+++ rocketsimulator/src/rocketsimulator/Simulator.java
@@ -11,8 +11,29 @@ package rocketsimulator;
  */
 public class Simulator {
     
+    private Rocket rocket = new Rocket();
+    
+    public void setFuelAmmount(double fuel){
+        rocket.setFuelAmmount(fuel);
+    }
+    
+    public void setFuelConsumption(double consumption){
+        rocket.setEngineConsumption(consumption);
+    }
+    
+    public void simu(){
+        setFuelAmmount(1);
+        setFuelConsumption(1);
+        double altitude;
+        double atmosphere = 60000;
+        
+        
+        rocket.go();
+        System.out.println(rocket.getHeight());
+    }
     
     public String launch() {
         return "laukaistu";
     }
+    
 }
